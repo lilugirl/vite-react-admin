37a186485f0ac693c36cae6acba8feffcca0470c
liu yi <lilugirl2005@126.com>
2021-04-19 17:53:22 +0800

    feat: 实现路由框架

---
+++ src/router/routes.ts
@@ -0,0 +1,13 @@
+import Home from '../views/home';
+import About from '../views/about';
+const routes=[
+  {
+      path:'/',
+      component:Home
+  },
+  {
+      path:'/about',
+      component:About
+  }
+];
+export default routes;
