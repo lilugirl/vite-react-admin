eac9d151ec1f759cca5220c723bf8457508aa10b
liu yi <lilugirl2005@126.com>
2021-04-19 18:03:09 +0800

    feat:引入antd，按需引入style

--- src/views/home.tsx
+++ src/views/home.tsx
@@ -1,5 +1,9 @@
 import React from 'react';
+import {Button} from 'antd';
 const Home=()=>{
-    return <div>Home Page</div>
+    return <div>
+        <h2>Home Page</h2>
+        <Button type="primary">按钮</Button>
+    </div>
 }
 export default Home;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import reactRefresh from '@vitejs/plugin-react-refresh'
+import vitePluginImp from 'vite-plugin-imp'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
+  plugins: [
+    reactRefresh(),
+    // 按需引入antd样式
+    vitePluginImp({
+      libList:[
+        {
+          libName:'antd',
+          style:(name)=>`antd/lib/${name}/style/index.less`
+        }
+      ]
+    })
+  ],
+  css:{
+    preprocessorOptions:{
+      less:{
+        // 支持内联Javascript
+        javascriptEnabled:true
+      }
+    }
+  }
 })
